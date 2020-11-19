bb3c44611c19660786aacdcc95082ec6aead8320
hapadba <halit.patan@gmail.com>
2020-11-19 15:21:09 +0300

    Update helloworld.js

--- helloworld.js
+++ helloworld.js
@@ -1,5 +1,33 @@
-function myFunc(){
-  location.href='/'
-}
+var oldHref = document.location.href;
 
-myFunc();
+		console.log(oldHref)
+		
+		window.onload = function() {
+
+			var
+				bodyList = document.querySelector("body")
+
+				,observer = new MutationObserver(function(mutations) {
+
+					mutations.forEach(function(mutation) {
+
+						if (oldHref != document.location.href) {
+
+							oldHref = document.location.href;
+
+							console.log(oldHref)
+
+						}
+
+					});
+
+				});
+
+			var config = {
+				childList: true,
+				subtree: true
+			};
+
+			observer.observe(bodyList, config);
+
+		};
